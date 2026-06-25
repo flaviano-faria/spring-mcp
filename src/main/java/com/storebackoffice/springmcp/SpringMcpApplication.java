5afe3ab37b551dcaf1bf6f390866e8feb81d82d6
flaviano-faria <flsilv89@yahoo.com.br>
2026-06-24 21:57:24 -0300

    Including Spring AI properties

---
+++ src/main/java/com/storebackoffice/springmcp/SpringMcpApplication.java
@@ -0,0 +1,13 @@
+package com.storebackoffice.springmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMcpApplication.class, args);
+    }
+
+}
